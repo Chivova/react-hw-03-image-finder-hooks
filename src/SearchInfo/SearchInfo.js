8230fad598b01ca348e07c67cb5fda665d34e2df
unknown <Chivovaz@ua.fm>
2021-07-07 22:07:26 +0300

    add spinner and  component Loader

--- src/SearchInfo/SearchInfo.js
+++ src/SearchInfo/SearchInfo.js
@@ -1,13 +1,16 @@
 import { Fragment, useState, useEffect } from 'react';
-// import Loader from 'react-loader-spinner';
+import Loader from 'react-loader-spinner';
 import imgApi from '../api/imgApi';
 import ImageGallery from '../ImageGallery';
 import Button from '../Button';
+import { toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SearchInfo({ imageQuery }) {
   const [gallery, setGallery] = useState([]);
   const [page, setPage] = useState(1);
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -15,23 +18,46 @@ export default function SearchInfo({ imageQuery }) {
       return;
     }
 
-    imgApi.fetchImgApi(imageQuery, page).then(gallery => {
-      if (gallery.length === 0) {
-        setError(`No images found on your request ${imageQuery}`);
-      }
-      // setLoading(true);
-      setGallery(state => [...state, ...gallery]);
-    });
-    // .finally(setLoading(false));
+    setLoading(true);
+
+    imgApi
+      .fetchImgApi(imageQuery, page)
+      .then(gallery => {
+        if (gallery.length === 0) {
+          setError(`No images found on your request ${imageQuery}`);
+
+          toast.error(error, {
+            position: 'top-right',
+            autoClose: 3000,
+          });
+        }
+
+        setGallery(state => [...state, ...gallery]);
+
+        // setLoading(false);
+      })
+      .finally(() => {
+        setLoading(false);
+        scroll();
+      });
   }, [imageQuery, error, page]);
 
   const updatePage = () => {
     setPage(page => page + 1);
   };
 
+  const scroll = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight - 1500,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <Fragment>
-      {/* <Loader type="Puff" color="#00BFFF" height={50} width={50} /> */}
+      {loading && (
+        <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+      )}
       <ImageGallery gallery={gallery} />
       <Button onClick={updatePage} />
     </Fragment>
